a8d3467bcaae1d57e5e22e73f9bc03c9397a5aa7
Volodymyr Pankiv <vpanki@softserveinc.com>
2024-08-01 18:27:00 +0300

    added hero section
    Signed-off-by: Volodymyr Pankiv <vpanki@softserveinc.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1s linear infinite",
+      },
+    },
+  },
   plugins: [
     require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
   ],
 };
